942b0309568e8bc5669d81bb954c853e40f908dc
zouheircadi <zouheir.cadi@gmail.com>
2016-12-04 13:47:14 +0100

    add customer tests

---
+++ catalog-rdbms/src/test/java/com/store/catalog/model/CustomerTest.java
@@ -0,0 +1,16 @@
+package com.store.catalog.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zouheir on 04/12/2016.
+ */
+public class CustomerTest {
+
+    @Test
+    public void testMatchPassword() throws Exception {
+        throw new RuntimeException("not yet implemented");
+    }
+}
