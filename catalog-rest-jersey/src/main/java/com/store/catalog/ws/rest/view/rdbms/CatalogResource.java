a71c07620ba793d79b6643267c1267443711ab83
TANG Kai <kai.tang@telecom-paristech.fr>
2017-01-30 16:29:51 +0100

    5.2 fait

--- catalog-rest-jersey/src/main/java/com/store/catalog/ws/rest/view/rdbms/CatalogResource.java
+++ catalog-rest-jersey/src/main/java/com/store/catalog/ws/rest/view/rdbms/CatalogResource.java
@@ -2,8 +2,13 @@ package com.store.catalog.ws.rest.view.rdbms;
 
 import com.store.catalog.model.ProductDTO;
 import com.store.catalog.service.catalog.CatalogService;
+
+import ch.qos.logback.core.status.Status;
+
 import com.store.catalog.model.CategoryDTO;
 import com.store.catalog.model.ItemDTO;
+
+import org.postgresql.translation.messages_bg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -32,7 +37,7 @@ public class CatalogResource {
     }
 
 
-//TO TEST: use http://localhost:8080/catalog/catalog/categories in navigator
+//TO TEST: use http://localhost:10080/catalog/catalog/categories in navigator
     @Path("/categories")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -40,7 +45,7 @@ public class CatalogResource {
     	return catalogServiceImpl.findCategories();
     }
 
-//TO TEST: http://localhost:8080/catalog/catalog/category/6 in navigator
+//TO TEST: http://localhost:10080/catalog/catalog/category/6 in navigator
     @Path("/category/{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -48,30 +53,44 @@ public class CatalogResource {
     	  return Response.ok(catalogServiceImpl.findCategory(id)).build();
     }
 
-    @Path("/category/create")
+    
+    @Path("/category")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createCategory(CategoryDTO categoryDTO) throws Exception {
-//TODO
-    	return null;
+    	return Response.ok(catalogServiceImpl.createCategory(categoryDTO)).build();
 
     }
 
-    @Path("/categories")
-    @GET
+    @Path("/category")
+    @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response updateCategory(CategoryDTO categoryDTO) throws Exception {
-
-    	return null;
+    	Long id = categoryDTO.getId();
+    	if( catalogServiceImpl.findCategory(id)!=null ) 
+    	{
+    		catalogServiceImpl.updateCategory(categoryDTO);
+    		return Response.ok(catalogServiceImpl.findCategory(id)).build();
+    	}
+    	else
+    		return Response.status(Status.ERROR).build();
 
     }
 
 
+    @Path("/category/{id}")
+    @DELETE
     public Response deleteCategory(@PathParam("id") Long id) throws Exception {
-
-    	return null;
+    	if( catalogServiceImpl.findCategory(id)!=null ) 
+    	{
+    		catalogServiceImpl.deleteCategory(id);;
+    		return Response.ok().build();
+    	}
+    	else
+    		return Response.status(Status.ERROR).build();
+    	
 
     }
 
